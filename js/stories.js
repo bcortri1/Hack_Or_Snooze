1e0ac6ab5eafdd1bf55712b5359f83303706fdaf
Blake C <bcortri1@kent.edu>
2023-09-07 19:51:17 -0400

    Only added remove button when able to be removed

--- js/stories.js
+++ js/stories.js
@@ -6,10 +6,10 @@ let storyList;
 /** Get and show stories when site first loads. */
 
 async function getAndShowStoriesOnStart() {
-  storyList = await StoryList.getStories();
-  $storiesLoadingMsg.remove();
+    storyList = await StoryList.getStories();
+    $storiesLoadingMsg.remove();
 
-  putStoriesOnPage();
+    putStoriesOnPage();
 }
 
 /**
@@ -20,18 +20,18 @@ async function getAndShowStoriesOnStart() {
  */
 
 function generateStoryMarkup(story) {
-  // console.debug("generateStoryMarkup", story);
+    // console.debug("generateStoryMarkup", story);
 
-  const hostName = story.getHostName();
-  //Allows UI to be different if logged in
-  let favHtml = "";
-  let removeHtml = "";
-  if (currentUser) {
-    const favorited = currentUser.isFavorite(story.storyId) ? "&starf;" : "&star;";
-    favHtml = `<span class="story-favorite">${favorited}</span>`
-    removeHtml = `<button class="story-remove">Remove</button>`
-  }
-  return $(`
+    const hostName = story.getHostName();
+    //Allows UI to be different if logged in
+    let favHtml = "";
+    let removeHtml = "";
+    if (currentUser) {
+        const favorited = currentUser.isFavorite(story.storyId) ? "&starf;" : "&star;";
+        favHtml = `<span class="story-favorite">${favorited}</span>`
+        removeHtml = currentUser.username === story.username ? `<button class="story-remove">Remove</button>`: null
+    }
+    return $(`
       <li id="${story.storyId}">
         ${favHtml}
         <a href="${story.url}" target="a_blank" class="story-link">
@@ -47,63 +47,63 @@ function generateStoryMarkup(story) {
 /** Gets list of stories from server, generates their HTML, and puts on page. */
 
 function putStoriesOnPage() {
-  console.debug("putStoriesOnPage");
+    console.debug("putStoriesOnPage");
 
-  $allStoriesList.empty();
+    $allStoriesList.empty();
 
-  //Loop through all of our stories and generate HTML for them
-  for (let story of storyList.stories) {
-    const $story = generateStoryMarkup(story);
-    $allStoriesList.append($story);
-  }
+    //Loop through all of our stories and generate HTML for them
+    for (let story of storyList.stories) {
+        const $story = generateStoryMarkup(story);
+        $allStoriesList.append($story);
+    }
 
-  $allStoriesList.show();
+    $allStoriesList.show();
 }
 
 //Sends storydata to api and updates page with stories
 async function storySubmission(evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  let storyData = {
-    author: $("#author-input").val(),
-    title: $("#title-input").val(),
-    url: $("#url-input").val()
-  }
+    let storyData = {
+        author: $("#author-input").val(),
+        title: $("#title-input").val(),
+        url: $("#url-input").val()
+    }
 
-  let story = await storyList.addStory(currentUser, storyData);
-  const $story = generateStoryMarkup(story);
-  $allStoriesList.prepend($story);
-  $storyForm[0].reset();
+    let story = await storyList.addStory(currentUser, storyData);
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.prepend($story);
+    $storyForm[0].reset();
 }
 
 $storySubmit.on("click", storySubmission);
 
 $allStoriesList.on("click", ".story-favorite", function (evt) {
-  let storyId = evt.target.parentElement.id;
-  if (currentUser.isFavorite(storyId)) {
-    currentUser.removeFavorite(storyId);
-    evt.target.innerHTML = "&star;"
-  }
-  else {
-    currentUser.addFavorite(storyId);
-    evt.target.innerHTML = "&starf;"
-  }
+    let storyId = evt.target.parentElement.id;
+    if (currentUser.isFavorite(storyId)) {
+        currentUser.removeFavorite(storyId);
+        evt.target.innerHTML = "&star;"
+    }
+    else {
+        currentUser.addFavorite(storyId);
+        evt.target.innerHTML = "&starf;"
+    }
 });
 
 $allStoriesList.on("click", ".story-remove", async function (evt) {
-  let storyId = evt.target.parentElement.id;
-  let res = await currentUser.removeStory(storyId);
-  if (res.status === 200) {
-    evt.target.parentElement.remove();
-  }
+    let storyId = evt.target.parentElement.id;
+    let res = await currentUser.removeStory(storyId);
+    if (res.status === 200) {
+        evt.target.parentElement.remove();
+    }
 })
 
-$navFavorites.on("click", function(){
-  hidePageComponents();
-  currentUser.putFavoritesOnPage();
+$navFavorites.on("click", function () {
+    hidePageComponents();
+    currentUser.putFavoritesOnPage();
 });
 
-$navOwn.on("click", function(){
-  hidePageComponents();
-  currentUser.putOwnOnPage();
+$navOwn.on("click", function () {
+    hidePageComponents();
+    currentUser.putOwnOnPage();
 });
